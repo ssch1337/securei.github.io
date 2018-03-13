8380fbbbf8a1848f543239893a5d5e31da1df9f1
securei <36975823+securei@users.noreply.github.com>
2018-03-13 18:26:37 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,48 +1,106 @@
-setInterval(function(){
-  $(document).ready(function() {
-    var h = $(this).height();
-    $('.preloader').height(h);
-  })
+var lineDrawing = anime.timeline({
+  direction: 'alternate',
+  loop: false
+}), svg = '.lineDrawing .svg path',
+btnanim = anime.timeline({
+  direction: 'alternate',
+  loop: false,
+  autoplay: false
 });
 
-$('.ml7 .letters').each(function(){
+$('.ml .letters').each(function(){
   $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
 });
 
-
-anime.timeline({loop: false})
+lineDrawing
   .add({
-    targets: '.ml7 .letter',
-    translateY: ["1.1em", 0],
-    translateX: ["0.55em", 0],
-    translateZ: 0,
-    rotateZ: [180, 0],
-    duration: 750,
-    easing: "easeOutExpo",
-    delay: function(el, i) {
-      return 50 * i;
+    targets: svg,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 2000,
+    delay: function(el, i){
+      return 100 * (i+1)
     }
-});
+  })
+  .add({
+    targets: svg,
+    fill: '#5E89FB',
+    duration: 1500
+  })
+  .add({
+    targets: svg,
+    translateY: [0,-400],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1000,
+    delay: function(el, i) {
+      return 100 + 50 * i;
+    }
+  })
+  .add({
+    targets: '.preloader',
+    zIndex: 6
+  })
+  .add({
+    targets: '.ml .line',
+    scaleY: [0,1],
+    opacity: [0.2,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml .line',
+    translateX: [0,$(".ml .letters").width()],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  })
+  .add({
+    targets: '.ml .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: function(el, i) {
+      return 20 * (i+1)
+    }
+  })
+  .add({
+    targets: '.buttn',
+    opacity: 1,
+    translateY: 0,
+    easing: "linear",
+    duration: 300
+  })
 
-setTimeout(function(){
-  anime.timeline({loop: false})
-    .add({
-      targets: '.btns',
-      opacity: 1,
-      duration: 1250,
-      delay: 0
-    })
-}, 1400);
+lineDrawing.complete = function(){
+  var fade = anime({
+    targets: '.ml .line',
+    opacity: [0,1],
+    duration: 500,
+    delay: 500,
+    direction: 'alternate',
+    loop: true
+  })
+}
 
-$('.btns').click(function () {
-  anime.timeline({loop: false})
-    .add({
-      targets: '.cp',
-      opacity: 0,
-      duration: 750,
-      easing: "easeOutExpo"
-    });
-  setTimeout(function(){
-    $('.cp').remove();
-  },800)
+btnanim
+  .add({
+    targets: '.preloader',
+    opacity: 0,
+    translateX: 500,
+    delay: 0,
+    easing: 'easeOutExpo'
+  })
+  .add({
+    targets: '.container',
+    translateX: [-500, 0],
+    opacity: 1,
+    delay: 0,
+    easing: 'easeOutExpo'
+  })
+
+$('.buttn').click(function(){
+  btnanim.play();
+  console.log('btn click')
 });
